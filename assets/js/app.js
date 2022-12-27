f9f99143968d678021ce2164cc123f9204a07629
zilva149 <114057257+zilva149@users.noreply.github.com>
2022-12-27 22:50:03 +0200

    modal settimeout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 const body = document.querySelector("body");
 const burgerMenu = document.querySelector(".burger-menu");
 const burgerClose = document.querySelector(".burger-close");
+const modal = document.querySelector(".modal");
+const modal_sm = document.querySelector(".modal-sm");
 
 if (burgerMenu) {
   burgerMenu.addEventListener("click", () => {
@@ -20,3 +22,15 @@ window.addEventListener("resize", () => {
     body.classList.remove("visible");
   }
 });
+
+if (modal) {
+  setTimeout(() => {
+    modal.remove();
+  }, 3000);
+}
+
+if (modal_sm) {
+  setTimeout(() => {
+    modal_sm.remove();
+  }, 3000);
+}
